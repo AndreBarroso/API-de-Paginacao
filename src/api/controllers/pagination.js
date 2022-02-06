885dcafb90da9800007a117d01d577661158b2b1
AndreBarroso <andre_barroso17@hotmail.com>
2022-02-06 13:44:41 -0300

    feat: criadas rotinas de retorno da api

--- src/api/controllers/pagination.js
+++ src/api/controllers/pagination.js
@@ -1,14 +1,17 @@
 const validations = require('../services/validations');
+const pagination = require('../services/pagination');
 
 const createArrayWithNumberOfPagination = async (req, res) => {
   const { currentPage, quantityPages } = req.body;
   const validateBody = validations.validateBody({ currentPage, quantityPages })
   if(validateBody) return res.status(400).json({error: validateBody});
 
+
+
   
  
 
-  res.status(201).json({ message: 'True' });
+  res.status(201).json(pagination.createArrayWithNumberOfPagination({currentPage, quantityPages}));
 };
 
 module.exports = {
--- src/api/services/pagination.js
+++ src/api/services/pagination.js
@@ -1,14 +1,56 @@
-const validations = require('./validations');
-
 const validations = require('../services/validations');
 
-const createArrayWithNumberOfPagination = async (req, res) => {
-  const { currentPage, quantityPages } = req.body;
+const generateArrayThatShowAll = (array, currentPage, quantityPages) => {
+  for(let index = 0; index < quantityPages; index += 1) {
+      let page = (index + 1).toString();
+      if (page == currentPage) page = `**${page}**`;
+      array.push(page);
+  } 
+}
 
-  return res.status(400).json({ message: 'Invalid entries. Try again.' });
- 
+const generateArrayThatShowParcialStartOrEndPages = (array, currentPage, quantityPages, limitNumbers) => {
+  if(currentPage <= parseInt( 1 + limitNumbers / 2)) {
+    for(let index = 0; index < limitNumbers ; index += 1) {
+      let page = (index + 1).toString();
+      if (page == currentPage) page = `**${page}**`;
+      array.push(page);
+    }
+    return array.push('...');
+  }
 
-  res.status(201).json(CreateRecipes);
+  array[0] = '...';
+  for(let index = 1; index <= limitNumbers; index += 1 ) {
+    let page = (quantityPages - limitNumbers + index).toString();
+    if (page == currentPage) page = `**${page}**`;
+    array.push(page);
+  }
+  return array;
+}
+
+const generateArrayWithcurrentPageInTheMidle = (array, currentPage, limitNumbers) => {
+  array.push('...');
+  for(let index = parseInt(currentPage - limitNumbers / 2); index < parseInt(currentPage + limitNumbers/ 2); index += 1 ) {
+    let page = (index + 1).toString();
+    if (page == currentPage) page = `**${page}**`;
+    array.push(page);
+  }
+  array.push('...');
+}
+
+const createArrayWithNumberOfPagination =  ({currentPage, quantityPages}) => {
+  const QUANTITY_NUMBERS_TO_SHOW = 5;
+  let pages = [];
+  if(quantityPages <= QUANTITY_NUMBERS_TO_SHOW) {
+    generateArrayThatShowAll(pages, currentPage, quantityPages);
+  }
+
+  else if(currentPage - parseInt( QUANTITY_NUMBERS_TO_SHOW / 2) > 1 && currentPage + parseInt( QUANTITY_NUMBERS_TO_SHOW / 2) < quantityPages) {
+    generateArrayWithcurrentPageInTheMidle(pages, currentPage, QUANTITY_NUMBERS_TO_SHOW);
+  }
+
+  else{generateArrayThatShowParcialStartOrEndPages(pages, currentPage, quantityPages, QUANTITY_NUMBERS_TO_SHOW);}
+
+  return pages;
 };
 
 module.exports = {
